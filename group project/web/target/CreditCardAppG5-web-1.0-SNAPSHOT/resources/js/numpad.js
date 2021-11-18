32496c70adb3bd3f93bc8aad81700fd9dc02e5e1
Radoslav22 <4takvr26@solent.ac.uk>
2021-11-18 23:34:57 +0000

    final commit

--- group project/web/target/CreditCardAppG5-web-1.0-SNAPSHOT/resources/js/numpad.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("crdkeypad").addEventListener("onclikc", e=>{
-   document.getElementById("numpadcvv").style.display = "none";
-   document.getElementById("numpadcredit").style.display="inline";
-});
